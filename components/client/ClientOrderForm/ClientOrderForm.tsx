dfbdfb841739552b5838f961fa8d4a6f4a3a4490
OlshePete <olshevskii.pv@gmail.com>
2024-06-14 09:22:34 +0300

    form

--- components/client/ClientOrderForm/ClientOrderForm.tsx
+++ components/client/ClientOrderForm/ClientOrderForm.tsx
@@ -13,127 +13,137 @@ import {
 import { Response, RouteSectionData } from '../../../types/webSiteContentTypes';
 const ClientOrderForm:FC<{routes:Response<RouteSectionData[]>}> = ({routes}) => {
   return (
-    <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
-    <Flex>
-      <Box
-        bg="#02054B"
-        color="white"
-        borderRadius="lg"
-        m={{ sm: 4, md: 16, lg: 10 }}
-        p={{ sm: 5, md: 5, lg: 16 }}>
-        <Box p={4}>
-          <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-            <WrapItem>
-              <Box>
-                <Heading>Оставить заявку</Heading>
-                <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                  Fill up the form below to contact
-                </Text>
-                <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                  <VStack pl={0} spacing={3} alignItems="flex-start">
-                    {
-                        routes.data.map((route,index)=>{
-                            return(
-                              <Button
-                              key={index}
-                      size="md"
-                      height="48px"
-                      width="200px"
-                      variant="ghost"
-                      color="#DCE2FF"
-                      _hover={{ border: '2px solid #1C6FEB' }}
-                      leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                      {route.attributes.name}
-                    </Button>  
-                            )
-                        })
-                    }
-                    
-                   
-                  </VStack>
-                </Box>
-                <HStack
-                  mt={{ lg: 10, md: 10 }}
-                  spacing={5}
-                  px={5}
-                  alignItems="flex-start">
-                  <IconButton
-                    aria-label="facebook"
-                    variant="ghost"
-                    size="lg"
-                    isRound={true}
-                    _hover={{ bg: '#0D74FF' }}
-                    icon={<MdFacebook size="28px" />}
-                  />
-                  <IconButton
-                    aria-label="github"
-                    variant="ghost"
-                    size="lg"
-                    isRound={true}
-                    _hover={{ bg: '#0D74FF' }}
-                    icon={<BsGithub size="28px" />}
-                  />
-                  <IconButton
-                    aria-label="discord"
-                    variant="ghost"
-                    size="lg"
-                    isRound={true}
-                    _hover={{ bg: '#0D74FF' }}
-                    icon={<BsDiscord size="28px" />}
-                  />
-                </HStack>
-              </Box>
-            </WrapItem>
-            <WrapItem>
-              <Box bg="white" borderRadius="lg">
-                <Box m={8} color="#0B0E3F">
-                  <VStack spacing={5}>
-                    <FormControl id="name">
-                      <FormLabel>Ваше имя</FormLabel>
-                      <InputGroup borderColor="#E0E1E7">
-                        <InputLeftElement
-                          pointerEvents="none"
-                        ><BsPerson color="gray.800" /></InputLeftElement>
-                        <Input type="text" size="md" />
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl id="name">
-                      <FormLabel>Номер телефона</FormLabel>
-                      <InputGroup borderColor="#E0E1E7">
-                        <InputLeftElement
-                          pointerEvents="none"
-                        ><MdOutlineEmail color="gray.800" /></InputLeftElement>
-                        <Input type="text" size="md" />
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl id="name">
-                      <FormLabel>Комментарий</FormLabel>
-                      <Textarea
-                        borderColor="gray.300"
-                        _hover={{
-                          borderRadius: 'gray.300',
-                        }}
-                        placeholder="message"
+<div className="container bg-white-600 w-dvw h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:mt-40">
+  <div className=" lg:col-span-2">
+<h1 className={styles.title}>Забронировать поездку</h1>
+    <div className="join join-vertical w-full p-48 pl-0">
+      {routes.data.map((route, index) => {
+        const defaultChecked=index===0
+        return(
+          <div 
+           key={index}
+          className="collapse  join-item border border-base-300">
+     <input type="radio" name="my-accordion-4" defaultChecked={defaultChecked} /> 
+     <div className={`${styles.route_title} collapse-title text-xl font-medium text-white`} >
+     {route.attributes.name}
+     </div>
+     <input type="checkbox"  className="checkbox collapse-arrow" />
+     <div className="collapse-content"> 
+       <p className={`${styles.text} text-white`}>{route.attributes.summary}</p>
+       <p>{route.attributes.price} руб.</p>
+     </div> 
+  
+   </div>)
+      })}
+    </div> 
+    <p>время </p>
+    <p>количество </p>
+  </div>
+  <div className="bg-teal">
+    <p>данные пользователя</p>
+  </div>
+  {/* <div className="flex">
+    <div className="bg-transparent text-white rounded-lg m-sm-4 m-md-16 m-lg-2 p-sm-5 p-md-5 p-lg-16">
+      <div className="p-4">
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <h2 className="text-2xl font-bold mb-3">Оставить заявку</h2>
+            <p className="text-gray-500 mb-5">Fill up the form below to contact</p>
+            <div className="py-5 md:py-8 lg:py-10">
+              <div className="flex flex-col space-y-3">
+                {routes.data.map((route, index) => (
+                  <button
+                    key={index}
+                    className="bg-transparent hover:border-2 hover:border-[#1C6FEB] text-[#DCE2FF] w-200px h-48px rounded-lg"
+                  >
+                    <span className="mr-2">
+                      <MdPhone size="20px" color="#1970F1" />
+                    </span>
+                    {route.attributes.name}
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="flex justify-start space-x-5 px-5 mt-10 md:mt-10">
+              <button
+                className="bg-transparent hover:bg-[#0D74FF] rounded-full"
+                aria-label="facebook"
+              >
+                <MdFacebook size="28px" />
+              </button>
+              <button
+                className="bg-transparent hover:bg-[#0D74FF] rounded-full"
+                aria-label="github"
+              >
+                <BsGithub size="28px" />
+              </button>
+              <button
+                className="bg-transparent hover:bg-[#0D74FF] rounded-full"
+                aria-label="discord"
+              >
+                <BsDiscord size="28px" />
+              </button>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <div className="bg-white rounded-lg p-8">
+              <div className="text-[#0B0E3F]">
+                <div className="space-y-5">
+                  <div className="flex flex-col">
+                    <label className="text-sm font-bold mb-2" >
+                      Ваше имя
+                    </label>
+                    <div className="flex items-center border border-[#E0E1E7] rounded-lg">
+                      <span className="mr-2">
+                        <BsPerson color="gray.800" />
+                      </span>
+                      <input
+                        type="text"
+                        className="w-full pl-10 pr-4 py-2 text-sm text-gray-700"
+                        id="name"
                       />
-                    </FormControl>
-                    <FormControl id="name" float="right">
-                      <Button
-                        variant="solid"
-                        bg="#0D74FF"
-                        color="white"
-                        _hover={{}}>
-                        Send Message
-                      </Button>
-                    </FormControl>
-                  </VStack>
-                </Box>
-              </Box>
-            </WrapItem>
-          </Wrap>
-        </Box>
-      </Box>
-    </Flex>
-  </Container>
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-sm font-bold mb-2" >
+                      Номер телефона
+                    </label>
+                    <div className="flex items-center border border-[#E0E1E7] rounded-lg">
+                      <span className="mr-2">
+                        <MdOutlineEmail color="gray.800" />
+                      </span>
+                      <input
+                        type="text"
+                        className="w-full pl-10 pr-4 py-2 text-sm text-gray-700"
+                        id="name"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-sm font-bold mb-2" >
+                      Комментарий
+                    </label>
+                    <textarea
+                      className="w-full pl-4 pr-4 py-2 text-sm text-gray-700 border border-gray-300 rounded-lg"
+                      placeholder="message"
+                    />
+                  </div>
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-[#0D74FF] text-white hover:bg-[#0D74FF] rounded-lg py-2 px-4"
+                    >
+                      Забронировать поездку
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> */}
+</div>
   )
 }
 
