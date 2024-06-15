9d24179866e95bfa481fdf5a524bfa93118d23c5
OlshePete <olshevskii.pv@gmail.com>
2024-06-15 23:17:50 +0300

    add side menu 4

--- components/server/TheHeader/OverlayMenu.tsx
+++ components/server/TheHeader/OverlayMenu.tsx
@@ -15,7 +15,16 @@ const OverlayMenu:FC<{}> = ({}) => {
 
   return (
    <>
-    <div className="fixed z-90 w-0 h-0 flex justify-center items-center bg-gray-900 opacity-0 duration-700" style={{ width: menuOpen ? '100vw' : '0', height: menuOpen ? '100vh' : '0', opacity: menuOpen ? 0.95 : 0 }}>
+    <div className="fixed z-90 w-0 h-0 flex justify-center items-center bg-gray-900 opacity-0 duration-700" 
+    style={{ 
+        width: menuOpen ? '100vw' : '0',
+         height: menuOpen ? '100vh' : '0',
+         opacity: menuOpen ? 0.95 : 0 ,
+         position: menuOpen ? "absolute":'inherit',
+         top:0,
+         left:0
+        }}
+    >
       <a href="#" className="fixed top-6 right-8 text-white hover:text-amber-500 text-7xl font-semibold duration-300" onClick={handleCloseMenu}>&times;</a>
       <div className="flex flex-col text-white text-center text-xl font-light space-y-3" onClick={handleCloseMenu}>
         <a className="hover:text-amber-500 duration-300" href="#top-routes">Валаам</a>
