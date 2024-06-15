c5fd5a5cead379fbf1d2122d4ebba7aae8959fbc
OlshePete <olshevskii.pv@gmail.com>
2024-06-15 13:47:46 +0300

    start section image styles && git push

--- components/server/sections/StartSection/StartSection.tsx
+++ components/server/sections/StartSection/StartSection.tsx
@@ -1,30 +1,33 @@
 import Image from "next/image";
-import { StartSectionData, Response } from "../../../../types/webSiteContentTypes";
+import {
+  StartSectionData,
+  Response,
+} from "../../../../types/webSiteContentTypes";
 import styles from "./StartSection.module.css";
 
-const StartSection = ({content}:{content:Response<StartSectionData>}) => {
-  const API_URL = process.env.API_URL
-  const {title, background, sub_title} = content.data.attributes;
-    return (
-      <section className={`${styles.container} section`}>
-        <div className={styles.title_container}>
-
+const StartSection = ({ content }: { content: Response<StartSectionData> }) => {
+  const API_URL = process.env.API_URL;
+  const { title, background, sub_title } = content.data.attributes;
+  return (
+    <section className={`${styles.container} section`}>
+      <div className={styles.title_container}>
         <h1 className={styles.title}>{title}</h1>
         {sub_title && <h2 className={styles.sub_title}>{sub_title}</h2>}
-        </div>
+      </div>
 
-        <Image
-          src={API_URL + (background.data.attributes.url|| "")}
-          alt={background.data.attributes.alternativeText || ""}
-          className={styles.image}
-          fill={true}
-          style={{
-            objectFit:'cover',
-          }}
-          quality={100}
-        />
-      </section>
-    );
-  };
-  
-  export { StartSection };
+      <Image
+        src={API_URL + (background.data.attributes.url || "")}
+        alt={background.data.attributes.alternativeText || ""}
+        className={styles.image}
+        fill={true}
+        sizes="100vw"
+        // style={{
+        //   objectFit: "cover",
+        // }}
+        // quality={100}
+      />
+    </section>
+  );
+};
+
+export { StartSection };
