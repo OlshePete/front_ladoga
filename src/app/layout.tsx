4a3f4750c28d66b67d27780f99a616d5d641a8fe
OlshePete <olshevskii.pv@gmail.com>
2024-10-08 00:25:33 +0300

    switch to local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from "next";
 import { Open_Sans, Dela_Gothic_One } from "next/font/google";
 import "./globals.scss";
-
-const deltaGO = Dela_Gothic_One({
-  subsets: ["cyrillic"],
-  weight: "400",
+import localFont from 'next/font/local'
+const deltaGO = localFont({
+  src:"public/Dela_Gothic_One/DelaGothicOne-Regular.ttf",
+  // subsets: ["cyrillic"],
+  // weight: "400",
   variable: "--font-header",
   preload: false,
 });
-const openSans = Open_Sans({
-  subsets: ["cyrillic"],
-  weight: ["400", "300", "700"],
-  variable: "--font-content",
-  preload: false,
-});
+// const openSans = localFont({
+//   src:"public/Open_Sans/DelaGothicOne-Regular.ttf",
+//   // subsets: ["cyrillic"],
+//   // weight: ["400", "300", "700"],
+//   variable: "--font-content",
+//   preload: false,
+// });
 
 export const metadata: Metadata = {
   title: "Магия севера",
@@ -27,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" style={{scrollBehavior:'smooth'}}>
-      <body className={`${deltaGO.variable} ${openSans.variable}`}>
+      <body className={`${deltaGO.variable} ${'openSans.variable'}`}>
         {children}
       </body>
     </html>
